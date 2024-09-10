fdb99ddf08fa634164cb27ee04079e263c828734
Tanasiev Sergiu <tanasiev.sergiu@gmail.com>
2024-09-10 10:21:13 +0200

    add field values to form data

--- client/src/data/formData.js
+++ client/src/data/formData.js
@@ -8,13 +8,55 @@ const chargePoints = {
         name: 'number_charge_points',
         title: 'number_charge_points',
         id: 'number_charge_points',
-        type: 'text',
+        type: 'number',
         placeholder: ''
     }
 }
 
+const arrivalProbability = {
+    id: 2,
+    tag: 'input',
+    data: {
+        text: 'Arrival probability',
+        name: 'arrival_probability',
+        title: 'arrival_probability',
+        id: 'arrival_probability',
+        type: 'number',
+        placeholder: ''
+    },
+}
+
+const consumption = {
+    id: 3,
+    tag: 'input',
+    data: {
+        text: 'Consumption',
+        name: 'consumption',
+        title: 'consumption',
+        id: 'consumption',
+        type: 'number',
+        placeholder: ''
+    },
+}
+
+const chargingPower = {
+    id: 4,
+    tag: 'input',
+    data: {
+        text: 'Charging Power (kW):',
+        name: 'charging_power',
+        title: 'charging_power',
+        id: 'charging_power',
+        type: 'number',
+        placeholder: ''
+    },
+}
+
 const ChargingStationForm = [
     chargePoints,
+    arrivalProbability,
+    consumption,
+    chargingPower
 ]
 
 
