98b12be9309f6ae5f422b1ec842dfea0a17a1939
Tanasiev Sergiu <tanasiev.sergiu@gmail.com>
2024-09-10 12:11:19 +0200

    fix concurently; add client to watch tailwind

--- client/src/components/input.js
+++ client/src/components/input.js
@@ -9,10 +9,12 @@ const Input = ({
     placeholder
 }) => {
     return (
-        <>
-            <label htmlFor={id}>{text}</label>
-            <input type={type} id={id} name={name} title={title} placeholder={placeholder} />
-        </>
+        <label htmlFor={id} class="form-control w-full max-w-xs">
+            <div class="label">
+                <span class="label-text">{text}</span>
+            </div>
+            <input type={type} id={id} name={name} title={title} placeholder={placeholder} class="input input-bordered input-sm w-full max-w-xs" />
+        </label>
     )
 }
 
